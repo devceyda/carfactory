1b778a9837c3c6dc86fed9666af2c5722d0dd996
Ceyda <ceydakuscuoglu@gmail.com>
2023-08-21 18:14:34 +0300

    -Update button action is done -New UI begin

--- src/main/java/com/carfactory/carfactory/controller/FactoryController.java
+++ src/main/java/com/carfactory/carfactory/controller/FactoryController.java
@@ -74,28 +74,14 @@ public class FactoryController {
         return "/Cars";
     }
 
-    @ModelAttribute("colors")
-    public List<Color> getColors() {
-        return colorService.getAllColor();
+    @RequestMapping(value = "/CarsDelete", method = RequestMethod.POST)
+    public String deleteCarByID(@RequestParam("carID") Integer id) {
+        carService.deleteCarByID(id);
+        return "/Cars";
     }
 
-    @ModelAttribute("brands")
-    public List<Brand> getBrands() {
-        return brandService.getAllBrand();
-    }
-
-    @GetMapping("/Cars/update/{id}")
-    public String updateCardForm(@PathVariable Integer id, Model model) {
-        model.addAttribute("Car", carService.getCarByID(id));
-        return "UpdateCar";
-    }
-
-    @PostMapping("/Cars/{id}")
-    public String updateCar(@PathVariable Integer id,
-            @ModelAttribute("Car") Car car,
-            Model model) {
-        // int CarID =Integer.parseInt(id);
-        // get student from database by id
+    @RequestMapping(value = "/UpdateCar", method = RequestMethod.POST)
+    public String updateCarByID(@ModelAttribute("Car") Car car, @RequestParam("id") Integer id) {
         Car existingCar = carService.getCarByID(id);
         existingCar.setCarID(id);
         existingCar.setColorID(car.getColorID());
@@ -109,23 +95,52 @@ public class FactoryController {
 
         // save updated student object
         carService.updateCar(existingCar);
-        return "redirect:/Cars";
+        return "/Cars";
     }
+
+    @ModelAttribute("colors")
+    public List<Color> getColors() {
+        return colorService.getAllColor();
+    }
+
+    @ModelAttribute("brands")
+    public List<Brand> getBrands() {
+        return brandService.getAllBrand();
+    }
+
+    // @GetMapping("/Cars/update/{id}")
+    // public String updateCardForm(@PathVariable Integer id, Model model) {
+    // model.addAttribute("Car", carService.getCarByID(id));
+    // return "UpdateCar";
+    // }
+
+    // @PostMapping("/Cars/{id}")
+    // public String updateCar(@PathVariable Integer id,
+    // @ModelAttribute("Car") Car car,
+    // Model model) {
+    // // int CarID =Integer.parseInt(id);
+    // // get student from database by id
+    // Car existingCar = carService.getCarByID(id);
+    // existingCar.setCarID(id);
+    // existingCar.setColorID(car.getColorID());
+    // existingCar.setBrandID(car.getBrandID());
+    // existingCar.setModel(car.getModel());
+    // existingCar.setPrice(car.getPrice());
+    // existingCar.setGearType(car.getGearType());
+    // existingCar.setFuelType(car.getFuelType());
+    // existingCar.setIsRefurbished(car.getIsRefurbished());
+    // existingCar.setReleaseDate(car.getReleaseDate());
+
+    // // save updated student object
+    // carService.updateCar(existingCar);
+    // return "redirect:/Cars";
+    // }
 
     // @GetMapping("/Cars/{id}")
     // public String deleteCar(@PathVariable Integer id) {
-    //     carService.deleteCarByID(id);
-    //     return "redirect:/Cars";
+    // carService.deleteCarByID(id);
+    // return "redirect:/Cars";
     // }
-
-    
-    @RequestMapping(value = "/CarsDelete", method = RequestMethod.POST)
-    public String deleteCarByID(@RequestParam("carID") Integer id) {
-        carService.deleteCarByID(id);
-        return "redirect:/Cars";
-    }
-
-
 
     // @PostMapping("/Cars")
     // public void SearchCar(
--- src/main/resources/static/JS/Cars.js
+++ src/main/resources/static/JS/Cars.js
@@ -1,43 +1,9 @@
-function getCar(car) {
-    console.log(car);
-    $("#modalTitle").text(car.brand + " " + car.model);
-    $("#brand").val(car.brand);
-    $("#model").val(car.model);
-    $("#color").val(car.color);
-    $("#price").val(car.formattedPrice);
-    if (car.gearType == "M") {
-        $("#gearType").val("Manuel");
-    } else if (car.gearType == "O") {
-        $("#gearType").val("Otomatic");
-    }
-    if (car.fuelType == "H") {
-        $("#fuelType").val("Hybrid");
-    } else if (car.fuelType == "D") {
-        $("#fuelType").val("Diesel");
-    } else if (car.fuelType == "F") {
-        $("#fuelType").val("Fuel");
-    } else if (car.fuelType == "E") {
-        $("#fuelType").val("Electric");
-    }
-    $("#releaseDate").val(car.formattedReleaseDate);
-    if (car.isRefurbished == false) {
-        $("#isRefurbished").val("No");
-    } else if (car.isRefurbished == true) {
-        $("#isRefurbished").val("Yes");
-    }
-    //$("#brand").val(car.find(x => x.carID == 65).brand);
-
-    $('#MdlCar').modal();
-}
-;
 $('#filter-form').on('submit', function (event) {
-
 
     console.log(form.serialize());
     var self = this;
     var form = $(this);
     var errorMsg = $('#errorMsg');
-
 
     if (form.data('requestRunning')) {
         return;
@@ -94,7 +60,7 @@ $(document).ready(function () {
                     targets: 0,
                     render: function (data, type, full, meta) {
                         const test = JSON.stringify(full);
-                        
+
                         var buttons = `
                         <a type="button" onclick='GetCarDetail(${test})' class="view-button"><i
                         class="fa-solid fa-lg fa-circle-chevron-right"></i>
@@ -105,18 +71,17 @@ $(document).ready(function () {
                 {
                     targets: -1,
 
-                    render: function (data,type,meta,full) {
-                        
-                       
-                        var buttons = `<a type="button" th:href="@{/Cars/update/{id}(id=${data.carID})}" class="btn action-button"
-                        style=" text-align:center; margin-right: 8px;" onclick="UpdateCar();">
+                    render: function (data, type, full, meta) {
+
+                        const test = JSON.stringify(full);
+                        var buttons = `<a type="button"  
+                        class="btn action-button"
+                        style=" text-align:center; margin-right: 8px;" onclick='UpdateCar(${test})'>
                         <i class="fa-solid fa-gear fa-sm" style=" text-align:center;"></i></a>
  
     
-                        <a type="button" th:href="@{/Cars/{id}(id=${data.carID})}"
-                        class="btn action-button delete-button" style="text-align:center;" onclick='deleteCarByID(${full.carID})'
-                        th:data-confirm-delete="|Are you sure you want to delete ${data.brand} ${data.model}?|"
-                        onclick="if (!confirm(this.getAttribute('data-confirm-delete'))) return false">
+                        <a type="button" 
+                        class="btn action-button delete-button" style="text-align:center;" onclick='deleteCarByID(${test})'>
                         <i class="fa-solid fa-trash-can fa-sm" style=" text-align:center;"></i></a>
     `;
                         return buttons;
@@ -132,6 +97,14 @@ $(document).ready(function () {
                         return "";
                     }
                 },
+                {
+                    targets: [4],
+                    render: 
+                    $.fn.dataTable.render.number( '.', ',', 2,""," TL" )
+                    
+
+
+                }
 
             ]
 
@@ -141,35 +114,149 @@ $(document).ready(function () {
 
 });
 
-function UpdateCar() {
-    $('#MdlUpdate').modal();
+function UpdateCar(car) {
+    $("#modalTitle").text(car.brand + " " + car.model);
+    $("#Brand").val(car.brandID);
+    $("#Model").val(car.model);
+    $("#Color").val(car.colorID);
+    $("#Price").val(car.price);
+    $("#GearType").val(car.gearType);
+    $("#FuelType").val(car.fuelType);
+    $("#ReleaseDate").val(car.releaseDate);
+    if (car.isRefurbished == false) {
+        document.getElementById("IsRefurbishedNo").checked = true;
+    } else if (car.isRefurbished == true) {
+        document.getElementById("IsRefurbishedYes").checked = true;
+    }
+    document.getElementById("Brand").disabled = false;
+    document.getElementById("Model").disabled = false;
+    document.getElementById("Color").disabled = false;
+    document.getElementById("Price").disabled = false;
+    document.getElementById("GearType").disabled = false;
+    document.getElementById("FuelType").disabled = false;
+    document.getElementById("ReleaseDate").disabled = false;
+    document.getElementById("IsRefurbishedNo").disabled = false;
+    document.getElementById("IsRefurbishedYes").disabled = false;
+    document.getElementById("save-button").style.visibility = 'visible';
+    //document.getElementById("save-button").addEventListener('click', save, false);
+    $('#MdlCar').modal();
+
+    $('#update-form').on('submit', function (event) {
+        var result = confirm("Want to Save?");
+        var self = this;
+        var form = $(this);
+        var errorMsg = $('#errorMsg');
+
+        var formData = form.serializeArray();
+        formData.push({ name: 'id', value: car.carID });
+        console.log(formData);
+
+        if (form.data('requestRunning')) {
+            return;
+        }
+
+        form.data('requestRunning', true);
+        event.preventDefault();
+
+        if (result) {
+            $.ajax({
+                url: form.attr("action"),
+                type: form.attr("method"),
+                data: formData,
+
+                success: function (result) {
+
+                    console.log("ajax working");
+                    window.location.reload();
+
+                },
+                complete: function (e) {
+                    form.data('requestRunning', false);
+                }
+
+            });
+        } else {
+            return;
+        }
+
+    });
 }
 
+// function save() {
+//     $('#update-form').on('submit', function (event) {
+
+//         console.log(form.serialize());
+//         var self = this;
+//         var form = $(this);
+//         var errorMsg = $('#errorMsg');
+
+//         if (form.data('requestRunning')) {
+//             return;
+//         }
+
+//         form.data('requestRunning', true);
+//         event.preventDefault();
+
+//         $.ajax({
+//             url: form.attr("action"),
+//             type: form.attr("method"),
+//             data: form.serialize(),
+//             success: function (result) {
+
+//                 console.log("ajax working");
+
+//             },
+//             complete: function (e) {
+//                 form.data('requestRunning', false);
+//             }
+
+//         });
+//         clear();
+//     });
+
+//     $('#MdlCar').modal();
+// }
 function GetCarDetail(car) {
     $("#modalTitle").text(car.brand + " " + car.model);
-    $("#brand").val(car.brand);
-    $("#model").val(car.model);
-    $("#color").val(car.color);
-    $("#price").val(car.formattedPrice);
-    if (car.gearType == "M") {
-        $("#gearType").val("Manuel");
-    } else if (car.gearType == "O") {
-        $("#gearType").val("Otomatic");
-    }
-    if (car.fuelType == "H") {
-        $("#fuelType").val("Hybrid");
-    } else if (car.fuelType == "D") {
-        $("#fuelType").val("Diesel");
-    } else if (car.fuelType == "F") {
-        $("#fuelType").val("Fuel");
-    } else if (car.fuelType == "E") {
-        $("#fuelType").val("Electric");
-    }
-    $("#releaseDate").val(car.formattedReleaseDate);
+    $("#Brand").val(car.brandID);
+    $("#Model").val(car.model);
+    $("#Color").val(car.colorID);
+    $("#Price").val(car.formattedPrice);
+    $("#GearType").val(car.gearType);
+    $("#FuelType").val(car.fuelType);
+    $("#ReleaseDate").val(car.releaseDate);
+    document.getElementById("Brand").disabled = true;
+    document.getElementById("Model").disabled = true;
+    document.getElementById("Color").disabled = true;
+    document.getElementById("Price").disabled = true;
+    document.getElementById("GearType").disabled = true;
+    document.getElementById("FuelType").disabled = true;
+    document.getElementById("ReleaseDate").disabled = true;
+    document.getElementById("IsRefurbishedNo").disabled = true;
+    document.getElementById("IsRefurbishedYes").disabled = true;
+    document.getElementById("save-button").style.visibility = 'hidden';
+    //$("#IsRefurbished").val(car.isRefurbished);
+    // if (car.gearType == "M") {
+    //     $("#GearType").val("Manuel");
+    // } else if (car.gearType == "O") {
+    //     $("#GearType").val("Otomatic");
+    // }
+    // if (car.fuelType == "H") {
+    //     $("#FuelType").val("Hybrid");
+    // } else if (car.fuelType == "D") {
+    //     $("#FuelType").val("Diesel");
+    // } else if (car.fuelType == "F") {
+    //     $("#FuelType").val("Fuel");
+    // } else if (car.fuelType == "E") {
+    //     $("#FuelType").val("Electric");
+    // }
+    //$("#ReleaseDate").val(car.formattedReleaseDate);
     if (car.isRefurbished == false) {
-        $("#isRefurbished").val("No");
+        document.getElementById("IsRefurbishedNo").checked = true;
+        //$("#IsRefurbishedNo").checked = true;
     } else if (car.isRefurbished == true) {
-        $("#isRefurbished").val("Yes");
+        document.getElementById("IsRefurbishedYes").checked = true;
+        //$("#IsRefurbishedYes").checked = true;
     }
     //$("#brand").val(car.find(x => x.carID == 65).brand);
 
@@ -177,17 +264,35 @@ function GetCarDetail(car) {
 }
 
 function deleteCarByID(data) {
-console.log(data)
-    $.ajax({
-        url: "/CarsDelete",
-        method: "POST",
-        data: data
-    }).done(function (data) {
-        console.log("ajax working");
-        console.log(data);
 
-    });
+    var result = confirm("Want to Delete "+ data.brand+" " + data.model +" ?");
+    if (result) {
+
+        console.log(data)
+        $.ajax({
+            url: "/CarsDelete",
+            method: "POST",
+            data: data
+        }).done(function (data) {
+            console.log("ajax working");
+            console.log(data);
+            window.location.reload();
+        });
+    } else {
+        return;
+    }
 
 
 }
 
+window.onscroll = function () {scrollFunction();};
+
+function scrollFunction() {
+if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+
+document.getElementById("navbar").style.background = "#501e27";
+} else {
+
+document.getElementById("navbar").style.background = "none";
+}
+}
